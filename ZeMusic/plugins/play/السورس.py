e260c8768c33dff1a0b5d4696a991ff1193cb4f6
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-06-07 19:51:13 +0300

    Update السورس.py

--- ZeMusic/plugins/play/السورس.py
+++ ZeMusic/plugins/play/السورس.py
@@ -1,39 +1,26 @@
 import asyncio
-
 import os
 import time
 import requests
-from pyrogram import filters
-import random
-from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from strings.filters import command
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from config import START_IMG_URL, OWNER_ID
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import app
-from random import  choice, randint
 
-                
-@app.on_message(
-    command(["سورس","‹ السورس ›"," ","السورس"])
-)
+@app.on_message(filters.text & filters.regex(r"^\السورس$"))
 async def huhh(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://graph.org/file/2f199ef466e7950dd5e79.jpg",
-        caption = f"""<b>  ⌯ 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 . .<b>\n<a href="https://t.me/EF_19"> ⌯ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙺𝙸𝙽𝙶 ⛧</a></b>""",
-reply_markup=InlineKeyboardMarkup(
+    dev = await client.get_users(OWNER_ID)
+    name = dev.first_name
+
+    await message.reply(
+        text=f"""<b>Dev ↠ <a href='tg://user?id={OWNER_ID}'>{name}</b></a>""",
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "‹ لتنصيب بوت ›", url=f"https://t.me/IC_19"),
-                ],[
-                    
-                
-                    InlineKeyboardButton(
-                        "‹ السورس ›", url=f"https://t.me/EF_19"),         
+                   InlineKeyboardButton(
+                        "𝚂𝙾𝚄𝚁𝙲𝙴 𝙺𝙸𝙽𝙶", url=f"https://t.me/EF_19"),
                 ],
-
             ]
-
         ),
-
+        reply_to_message_id=message.id  # This ensures the bot replies to the user s message
     )
