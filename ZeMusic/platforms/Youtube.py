e4c816ceca457bbb57fa222902251b910210a8b2
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-08-07 19:32:47 +0300

    Update Youtube.py

--- ZeMusic/platforms/Youtube.py
+++ ZeMusic/platforms/Youtube.py
@@ -177,11 +177,7 @@ class YouTubeAPI:
             link = self.base + link
         if "&" in link:
             link = link.split("&")[0]
-        ytdl_opts = {
-            'quiet': True,
-            'headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-        }
+        ytdl_opts = {"quiet": True}
         ydl = yt_dlp.YoutubeDL(ytdl_opts)
         with ydl:
             formats_available = []
