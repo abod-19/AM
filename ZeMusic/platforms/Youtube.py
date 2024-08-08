ae3dd31a9d64cc67cb2573453cf5d961349bc0e9
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-08-08 11:35:50 +0300

    Update Youtube.py

--- ZeMusic/platforms/Youtube.py
+++ ZeMusic/platforms/Youtube.py
@@ -249,6 +249,7 @@ class YouTubeAPI:
                 "nocheckcertificate": True,
                 "quiet": True,
                 "no_warnings": True,
+                "cookiefile": "cookies.txt",
             }
             x = yt_dlp.YoutubeDL(ydl_optssx)
             info = x.extract_info(link, False)
